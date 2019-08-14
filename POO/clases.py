2c7164bd5c9939a55ba5a2b412c43275647dc4c9
Jeff <jeff.uca@gmail.com>
2019-08-14 22:41:38 +0200

    Encapsulación de metodos

--- POO/clases.py
+++ POO/clases.py
@@ -7,12 +7,29 @@ class Coche():
         self.__enmarcha = False
 
     def arrancar(self, arrancar):
-        self.enmarcha = arrancar
-        if arrancar:
+        self.__enmarcha = arrancar
+
+        if self.__enmarcha:
+            chequeo = self.__chequeo_interno()
+        if arrancar and chequeo :
             print("Coche en marcha.")
+        elif self.__enmarcha == True and not chequeo:
+            print("No podemos arrancar, chequeo incorrecto.")
         else:
             print("Se ha parado el coche.")
 
     def estado(self):
         print("El coche tiene un largo de {} cm \n un ancho de {} cm \n tiene {} ruedas \n".format(
             self.largoChasis, self.anchoChasis, self.ruedas))
+
+    def __chequeo_interno(self):
+        print("Realizado chequeo interno")
+        self.gasolina = "ok"
+        self.aceite ="ok"
+        self.puertas ="cerradas"
+
+        if self.gasolina =="ok" and self.aceite =="ok" and self.puertas =="cerradas":
+            return True
+        else:
+            return False
+
