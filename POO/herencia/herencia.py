3395392c2af963697185174bdc8dae8fa4199d91
Jeff <jeff.uca@gmail.com>
2019-08-15 14:35:04 +0200

    Agredos más métodos a la clase vehículo

--- POO/herencia/herencia.py
+++ POO/herencia/herencia.py
@@ -1,4 +1,7 @@
 class Vehiculo():
+    LIMITE_VELOCIDAD = 300
+    direcciones = ('recto', 'marcha atras', 'izquierda', 'derecha')
+
     def __init__(self, marca, modelo):
         self.marca = marca
         self.modelo = modelo
@@ -6,20 +9,45 @@ class Vehiculo():
         self.acelera = False
         self.frena = False
         self.velocidad = 0
+        self.direccion = 'recto'
 
     def arrancar(self):
         self.enmarcha = True
 
     def acelerar(self):
-        self.acelera = True
+
+        if not self.acelera:
+            self.acelera = True
+            self.frena = False
+
+        if self.velocidad < Vehiculo.LIMITE_VELOCIDAD:
+            self.velocidad += 1
 
     def frenar(self):
-        self.frena = True
+
+        if not self.frena:
+            self.frena = True
+            self.acelera = False
+
+        if self.velocidad > 0:
+            self.velocidad -= 1
+
+    def girar(self, direccion):
+
+        if direccion in Vehiculo.direcciones:
+            self.direccion = direccion
+            if direccion == "derecha":
+                print("Girando a la derecha.")
+            if direccion == "izquierda":
+                print("Girando a la derecha.")
+        else:
+            print('dirección incorrecta pruebe [recto, derecha, izquierda, marcha atras]')
+
+
 
     def estado(self):
         print("Marca: {} \nModelo: {}\nArrancado: {} \nAcelerando: {} \nFrenando: {} "
               .format(self.marca, self.modelo, self.arrancar(), self.acelera, self.frena))
-
 
 
 class Moto(Vehiculo):
