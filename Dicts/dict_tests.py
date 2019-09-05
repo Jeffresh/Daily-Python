3f67a5b3718949761853118486b9dd2530a7fd4c
Jeff <jeff.uca@gmail.com>
2019-09-05 22:01:00 +0200

    Some dicts exercises resolved

--- Dicts/dict_tests.py
+++ Dicts/dict_tests.py
@@ -1,12 +1,21 @@
-funcion = { 'Computable': ["suma","multipicación","potencia","factorial"], 'No computable':{'nombre':["resta",'division']}}
+funcion = {'Computable': ["suma","multipicación","potencia","factorial"], 'No computable':{'nombre':["resta",'division']}}
 
+# CHALLENGE!
+#Use a dictionary comprehension to count the length of each word in a sentence.
+
+sentence = "All that is gold does not glitter,Not all those who wander are lost"
+
+
+#For all the numbers 1-1000, use a nested list/dictionary comprehension to
+#find the highest single digit any of the numbers is divisible by.
 
 
 
 if __name__ == '__main__':
+    result = {word: len(word) for word in sentence.split()}
 
-    pistola =None
-    k = list(funcion['No computable'].keys())
+    print(result)
 
-    for i in k:
-        print(i)
+    result = {number:max([divisor for divisor in range(1, 10) if number % divisor == 0]) for number in range(1, 1001)}
+
+    print(result)
