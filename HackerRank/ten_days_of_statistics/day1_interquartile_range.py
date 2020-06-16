14201ce7cc76addd1a9bc2e8b52683791c8d5d69
Jeff <jeff.uca@gmail.com>
2020-06-16 15:23:35 +0200

    :art: add coments to interquartile

--- HackerRank/ten_days_of_statistics/day1_interquartile_range.py
+++ HackerRank/ten_days_of_statistics/day1_interquartile_range.py
@@ -1,3 +1,16 @@
+# Task
+# The interquartile range of an array is the difference between its first () and third () quartiles (i.e.,
+#
+# ).
+#
+# Given an array,
+# , of integers and an array, , representing the respective frequencies of 's elements, construct a data set, , where each occurs at frequency . Then calculate and print 's interquartile range, rounded to a scale of decimal place (i.e.,
+#
+# format).
+#
+# Tip: Be careful to not use integer division when averaging the middle two elements for a data set with an even number of elements, and be sure to not include the median in your upper and lower data sets.
+#
+
 def quartiles(l):
     l.sort()
     size = len(l)
