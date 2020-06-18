a71046168ea3cb159c0089ffff30071551b60e31
Jeff <jeff.uca@gmail.com>
2020-06-18 20:49:38 +0200

    :art: format code

--- HackerRank/ten_days_of_statistics/day_3_cards_of_the_same_suit.py
+++ HackerRank/ten_days_of_statistics/day_3_cards_of_the_same_suit.py
@@ -5,18 +5,18 @@
 if __name__ == '__main__':
     cards = 52
     suits = 4
-    cards_per_suits = 52/4
+    cards_per_suits = 52 / 4
 
     print(cards_per_suits)
 
-    #first draw
+    # first draw
 
     cards_after_first_draw = 51
-    cards_of_suit_of_draw_chosen = (52/4) - 1
+    cards_of_suit_of_draw_chosen = (52 / 4) - 1
 
     print(cards_per_suits)
 
-    #second draw - probability of a card will be of the same suit -
+    # second draw - probability of a card will be of the same suit -
 
     p_two_same_suit = cards_of_suit_of_draw_chosen / cards_after_first_draw
 
