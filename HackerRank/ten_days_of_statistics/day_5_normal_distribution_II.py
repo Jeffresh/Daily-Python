0045ad1c965b370a9cb1e8d46a36ef9c93cb4375
Jeff <jeff.uca@gmail.com>
2020-06-20 13:32:30 +0200

    :sparkles: day 5 hacker rank normal distribution II

--- HackerRank/ten_days_of_statistics/day_5_normal_distribution_II.py
+++ HackerRank/ten_days_of_statistics/day_5_normal_distribution_II.py
@@ -22,7 +22,28 @@ def cumulative_probability_normal_distribution(mean, std, x):
     return (1 + erf((x - mean) / (std * sqrt(2)))) / 2
 
 
+
 if __name__ == '__main__':
     normal_mean, normal_std = map(float, input().split())
     grade_to_higher = float(input())
     grade_to_pass = float(input())
+
+    max_grade = 100
+
+    # P Scored higher than grade
+
+    p_grade_lt_80 = cumulative_probability_normal_distribution(normal_mean, normal_std, grade_to_higher)
+    p_grade_gt_80 = 1 - p_grade_lt_80
+    print('{0:.02f}'.format(p_grade_gt_80 * 100))
+
+    # P Scored higher or equal than grade ( pass the exam)
+
+    p_grade_range = cumulative_probability_normal_distribution(normal_mean , normal_std, grade_to_pass)
+    p_passed = 1 - p_grade_range
+    print('{0:.02f}'.format(p_passed * 100))
+
+    # P Scored less than a grade ( failed the exam)
+
+    print('{0:.02f}'.format(p_grade_range * 100))
+
+
