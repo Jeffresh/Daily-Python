2ace5cf953692599615a432821fa36cbb3f6627c
Jeff <jeff.uca@gmail.com>
2020-06-20 11:55:16 +0200

    :sparkles: add hacker rank poisson distribution II

--- HackerRank/ten_days_of_statistics/day_5_poisson_distribution_II.py
+++ HackerRank/ten_days_of_statistics/day_5_poisson_distribution_II.py
@@ -9,3 +9,12 @@
 # The number of repairs Y, that machine B needs is a Poisson random variable with mean 1.55 .
 # The daily cost of operating B is CB = 128 + 40 Y^2 .
 #
+
+
+if __name__ == "__main__":
+    mean_a, mean_b = map(float, input().split())
+
+    CA = 160 + 40 * (mean_a + mean_a ** 2)
+    CB = 128 + 40 * (mean_b + mean_b ** 2)
+    print('{0:.03f}'.format(CA))
+    print('{0:.03f}'.format(CB))
