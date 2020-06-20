80d1d8129828a42bb8db1a2cbe6a8a538b1fdd70
Jeff <jeff.uca@gmail.com>
2020-06-20 12:10:04 +0200

    :sparkles: add task hacker rank day 5 normal distribution I

---
+++ HackerRank/ten_days_of_statistics/day_5_normal_distribution.py
@@ -0,0 +1,7 @@
+# Task
+# In a certain plant, the time taken to assemble a car is a random variable, X
+# having a normal distribution with a mean 20 of hours and a standard deviation of
+# 2 hours. What is the probability that a car can be assembled at this plant in:
+
+# Less than 19.5 hours?
+# Between 20 and 22 hours?
