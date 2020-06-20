49cd4ae01c7817f14a49457f62ba154977785c2a
Jeff <jeff.uca@gmail.com>
2020-06-20 11:28:57 +0200

    :sparkles: add day 5 hacker rank poisson distribution

--- HackerRank/ten_days_of_statistics/day_5_poisson_distribution.py
+++ HackerRank/ten_days_of_statistics/day_5_poisson_distribution.py
@@ -1,3 +1,18 @@
 # Task
 # A random variable X , follows Poisson distribution with mean of 2.5
 # Find the probability with which the random variable X is equal to 5.
+
+from math import factorial
+
+
+def poisson_distribution(k, mean):
+    return (mean ** k * 2.71828 ** (-mean)) / factorial(k)
+
+
+if __name__ == "__main__":
+    mean = float(input())
+    variable_value = float(input())
+
+    p = poisson_distribution(variable_value, mean)
+
+    print('{0:.03f}'.format(p))
