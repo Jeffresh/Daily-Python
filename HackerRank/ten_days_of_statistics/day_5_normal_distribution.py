bef92d830df43d4ccec348eff0cf3e742bbfc0df
Jeff <jeff.uca@gmail.com>
2020-06-20 12:50:17 +0200

    :sparkles: add day 5 hacker rank normal distribution

--- HackerRank/ten_days_of_statistics/day_5_normal_distribution.py
+++ HackerRank/ten_days_of_statistics/day_5_normal_distribution.py
@@ -5,3 +5,30 @@
 
 # Less than 19.5 hours?
 # Between 20 and 22 hours?
+
+from math import sqrt, pi, exp, erf
+
+
+def normal_distribution(mean, std, x):
+    return (1 / (std * sqrt(2 * pi))) * exp((- (x - mean) ** 2) / (2 * std ** 2))
+
+
+def cumulative_probability_normal_distribution(mean, std, x):
+    return (1 + erf((x - mean) / (std * sqrt(2))))/2
+
+
+if __name__ == '__main__':
+    normal_mean, normal_std = map(float, input().split())
+    value = float(input())
+    range_a, range_b = map(float, input().split())
+
+    # P less than 19.5
+    p_lt = cumulative_probability_normal_distribution(normal_mean, normal_std, value)
+    print('{0:.03f}'.format(p_lt))
+
+    #P between 20 22
+    p_range_a = cumulative_probability_normal_distribution(normal_mean, normal_std, range_a)
+    p_range_b = cumulative_probability_normal_distribution(normal_mean, normal_std, range_b)
+    p_range = p_range_b - p_range_a
+    print('{0:.03f}'.format(p_range))
+
