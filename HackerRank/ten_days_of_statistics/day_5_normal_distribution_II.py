9651cf34adfc7334bf86c885a0059338d16bfaeb
Jeff <jeff.uca@gmail.com>
2020-06-20 13:00:23 +0200

    :sparkles: add task of day 5 normal distribution II

---
+++ HackerRank/ten_days_of_statistics/day_5_normal_distribution_II.py
@@ -0,0 +1,12 @@
+# Task
+
+# The final grades for a Physics exam taken by a large group of students have a mean 70 of
+# and a standard deviation of 10
+#
+# If we can approximate the distribution of these grades by a normal distribution, what percentage of the students:
+#
+# 1. Scored higher than 80
+# 2. Passed the test ( grade >= 60)
+# 3. Failed the test ( grade < 60)
+#
+# Find and print the answer to each question on a new line, rounded to a scale of 2 decimal places.
