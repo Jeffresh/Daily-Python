4feb54b5d1e88eea9cb21ae11825946a17fb6f2d
Jeff <jeff.uca@gmail.com>
2020-06-20 11:49:00 +0200

    :sparkles: add task day 5 poisson distribution II

---
+++ HackerRank/ten_days_of_statistics/day_5_poisson_distribution_II.py
@@ -0,0 +1,11 @@
+# Task
+# The manager of a industrial plant is planning to buy a machine of either type A or type B.
+#
+# For each day’s operation:
+#
+# The number of repairs X, that machine A needs is a Poisson random variable with mean 0.88 .
+# The daily cost of operating A is CA = 160 + 40 X^2 .
+
+# The number of repairs Y, that machine B needs is a Poisson random variable with mean 1.55 .
+# The daily cost of operating B is CB = 128 + 40 Y^2 .
+#
