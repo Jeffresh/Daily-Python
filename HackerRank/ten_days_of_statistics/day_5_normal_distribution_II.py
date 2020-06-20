aef2055571eb19d68254aceb68951d9222fd7e5d
Jeff <jeff.uca@gmail.com>
2020-06-20 13:03:41 +0200

    :sparkles: add functions of normal distribution  and cumulative probability to day 5 hacker rank normal distribution II

--- HackerRank/ten_days_of_statistics/day_5_normal_distribution_II.py
+++ HackerRank/ten_days_of_statistics/day_5_normal_distribution_II.py
@@ -10,3 +10,19 @@
 # 3. Failed the test ( grade < 60)
 #
 # Find and print the answer to each question on a new line, rounded to a scale of 2 decimal places.
+
+from math import sqrt, pi, exp, erf
+
+
+def normal_distribution(mean, std, x):
+    return (1 / (std * sqrt(2 * pi))) * exp((- (x - mean) ** 2) / (2 * std ** 2))
+
+
+def cumulative_probability_normal_distribution(mean, std, x):
+    return (1 + erf((x - mean) / (std * sqrt(2)))) / 2
+
+
+if __name__ == '__main__':
+    normal_mean, normal_std = map(float, input().split())
+    grade_to_higher = float(input())
+    grade_to_pass = float(input())
