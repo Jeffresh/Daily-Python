65ac1a280e7ace862d5217cf048174d8d5eaa379
Jeff <jeff.uca@gmail.com>
2020-06-21 13:37:57 +0200

    :sparkles: add task of day 6 hacker rank the central limit theorem III

---
+++ HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_III.py
@@ -0,0 +1,6 @@
+# Task
+# You have a sample of 100 values from a population with mean 500 and with standard deviation 80 .
+# Compute the interval that covers the middle 95% of the distribution of the sample mean; in other words,
+# compute A and B such that P(A < x < B) = 0.95. Use the value of z = 1.96
+
+
