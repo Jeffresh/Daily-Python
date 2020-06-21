dc4d3acaa99134dcf448a97b5c069d1a402b1944
Jeff <jeff.uca@gmail.com>
2020-06-21 14:56:13 +0200

    :sparkles: hacker rank day 6 the central limit theorem III, intervals

--- HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_III.py
+++ HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_III.py
@@ -4,3 +4,28 @@
 # compute A and B such that P(A < x < B) = 0.95. Use the value of z = 1.96
 
 
+from math import sqrt
+
+if __name__ == '__main__':
+    n = int(input())
+    mean = int(input())
+    std = int(input())
+    interval = float(input())
+    z = float(input())
+
+    # using z value
+
+    # mean of sample mean  = mean, deviation of sample mean  =  deviation / sqrt(n)
+
+    sqrt_n = sqrt(n)
+
+    sample_mean = mean
+    sample_deviation = std / sqrt_n
+
+    # prediction invervals using z value (standard score) of a distribution of the sample mean
+
+    pl = sample_mean - sample_deviation * z
+    pr = sample_mean + sample_deviation * z
+
+    print('{0:.2f}'.format(pl))
+    print('{0:.2f}'.format(pr))
