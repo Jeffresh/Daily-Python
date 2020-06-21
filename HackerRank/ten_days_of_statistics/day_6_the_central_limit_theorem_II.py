234cc4e49ada32a3707138e87b10530328d14020
Jeff <jeff.uca@gmail.com>
2020-06-21 12:43:15 +0200

    :sparkles: add day 6 hacker rank the central limit theorem II

--- HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_II.py
+++ HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_II.py
@@ -4,3 +4,21 @@
 # A few hours before the game starts, 100 eager students line up to purchase last-minute tickets.
 # If there are only 250 tickets left, what is the probability that all 100 students will be able to purchase tickets?
 
+from math import erf, sqrt
+
+
+def cumulative_probability_normal_distribution(mean, std, x):
+    return (1 + erf((x - mean) / (std * sqrt(2)))) * 0.5
+
+
+if __name__ == '__main__':
+    max_tickets = 250
+    students = 100
+    mean_sells = 2.4
+    std_sells = 2.0
+
+    sum_mean = students * mean_sells
+    sum_std = sqrt(students) * std_sells
+
+    pr = cumulative_probability_normal_distribution(sum_mean, sum_std, max_tickets)
+    print('{0:.04f}'.format(pr))
