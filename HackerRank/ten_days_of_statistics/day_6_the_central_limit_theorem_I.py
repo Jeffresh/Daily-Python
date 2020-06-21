5b4bb05d20ac1bf23de11b33869656d8bda03414
Jeff <jeff.uca@gmail.com>
2020-06-21 12:12:01 +0200

    :sparkles: hacker day 6 the central limit theorem I

--- HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_I.py
+++ HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_I.py
@@ -3,9 +3,22 @@
 # and a standard deviation of 15 pounds. Based on this information, what is the probability that all 49 boxes can be
 # safely loaded into the freight elevator and transported?
 
+from math import erf, sqrt
+
+
+def cumulative_probability_normal_distribution(mean, std, x):
+    return (1 + erf((x - mean) / (std * sqrt(2)))) * 0.5
+
+
 if __name__ == '__main__':
     maximum_pounds = 9800
     max_cargo = 49
     mean = 205
     std = 15
 
+    mu_sum = max_cargo * mean
+    std_sum = sqrt(max_cargo) * std
+
+    pr = (round(cumulative_probability_normal_distribution(mu_sum, std_sum, maximum_pounds), 4))
+
+    print(pr)
