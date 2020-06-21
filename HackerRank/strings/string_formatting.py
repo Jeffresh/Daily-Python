66c53a17327016af2fcbe6ea3dcb2afe75e1ae5d
Jeff <jeff.uca@gmail.com>
2020-06-21 16:55:15 +0200

    :pencil2: typo on width

--- HackerRank/strings/string_formatting.py
+++ HackerRank/strings/string_formatting.py
@@ -9,7 +9,7 @@
 
 
 def print_formatted(number):
-    width = len("{0:b}".format(n))
+    width = len("{0:b}".format(number))
     output = [
         '{0:{width}d} {0:{width}o} {0:{width}X} {0:{width}b}'.format(i, width=width) for
         i in range(1, number + 1)]
