3aa9ab838e1990e357f7ed9bf33a72a085e8f2b2
Jeff <jeff.uca@gmail.com>
2020-06-21 10:26:42 +0200

    :sparkles: add task central limit theorem day 6  hacker rank

---
+++ HackerRank/ten_days_of_statistics/day_6_the_central_limit_theorem_I.py
@@ -0,0 +1,11 @@
+# Task A large elevator can transport a maximum 9800 of pounds. Suppose a load of cargo containing 49 boxes must be
+# transported via the elevator. The box weight of this type of cargo follows a distribution with a mean of pounds 205
+# and a standard deviation of 15 pounds. Based on this information, what is the probability that all 49 boxes can be
+# safely loaded into the freight elevator and transported?
+
+if __name__ == '__main__':
+    maximum_pounds = 9800
+    max_cargo = 49
+    mean = 205
+    std = 15
+
