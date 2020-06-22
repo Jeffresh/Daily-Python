2b83dc4af4bed880225848a248e83940f0133edf
Jeff <jeff.uca@gmail.com>
2020-06-22 14:15:07 +0200

    :sparkles: add task of day 7 spearman rank correlation

---
+++ HackerRank/ten_days_of_statistics/day_7_spearman_rank_correlation.py
@@ -0,0 +1,9 @@
+# Task
+# Given two n -element data sets, X and Y calculate the value of Spearman's rank correlation coefficient.
+#
+# Input Format
+#
+# The first line contains an integer, n , denoting the number of values in data sets X and Y .
+# The second line contains n space-separated real numbers (scaled to at most one decimal place) denoting data set X.
+# The third line contains n space-separated real numbers (scaled to at most one decimal place) denoting data set Y .
+
