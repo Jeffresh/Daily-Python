eaa2e91561d85e6271e0d80b44cab08320897bce
Jeff <jeff.uca@gmail.com>
2020-06-22 15:07:35 +0200

    :sparkles: add definitions spearman rank correlation coefficient

--- HackerRank/ten_days_of_statistics/day_7_spearman_rank_correlation.py
+++ HackerRank/ten_days_of_statistics/day_7_spearman_rank_correlation.py
@@ -7,3 +7,7 @@
 # The second line contains n space-separated real numbers (scaled to at most one decimal place) denoting data set X.
 # The third line contains n space-separated real numbers (scaled to at most one decimal place) denoting data set Y .
 
+# rs (spearman rank correlation coefficient) is equal to Pearson correlation coefficient of Rankx and Ranky.
+# rank is equal of the order on the list.
+# Special case if X and Y dont contain duplicates then rs = 1- (6 * sum(rankxi - rankxj))/(n*n^2 -1)
+
