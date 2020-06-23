bfcd329505e7ea5564d75e0229377a60eb056306
Jeff <jeff.uca@gmail.com>
2020-06-23 15:27:26 +0200

    :sparkles: task day 8 least square regression

---
+++ HackerRank/ten_days_of_statistics/day_8_least_square_regression_line.py
@@ -0,0 +1,9 @@
+# Task
+# A group of five students enrolls in Statistics immediately after taking a Math aptitude test.
+# Each student's Math aptitude test score, x, and Statistics course grade, y,
+# can be expressed as the following list of (x,y) points.
+# Determine the equation of the best-fit line using the least squares method, then compute and print the value of y
+# when x = 80.
+
+if __name__ == '__main__':
+    pass
