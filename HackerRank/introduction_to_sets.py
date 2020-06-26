ac4732002263adcf83bbbb2d7a2ec6f0acb58fe4
Jeff <jeff.uca@gmail.com>
2020-06-26 10:09:29 +0200

    :sparkles: solution hacker rank introduction to sets

--- HackerRank/introduction_to_sets.py
+++ HackerRank/introduction_to_sets.py
@@ -3,4 +3,19 @@
 # Now, let's use our knowledge of sets and help Mickey.
 #
 # Ms. Gabriel Williams is a botany professor at District College.
-# One day, she asked her student Mickey to compute the average of all the plants with distinct heights in her greenhouse.
+# One day, she asked her student Mickey to compute the average of
+# all the plants with distinct heights in her greenhouse.
+
+
+def average(array):
+    data = set(array)
+    len_data = len(data)
+    mean = sum(data) / len_data
+    return mean
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list(map(int, input().split()))
+    result = average(arr)
+    print(result)
