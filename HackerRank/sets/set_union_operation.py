70aec3a8c30787f1e39a82505651ad309c750ab7
Jeff <jeff.uca@gmail.com>
2020-06-27 10:15:01 +0200

    :sparkles: add set union operation hacker rank

---
+++ HackerRank/sets/set_union_operation.py
@@ -0,0 +1,17 @@
+# Task
+#
+# The students of District College have subscriptions to English and French newspapers.
+# Some students have subscribed only to English,
+# some have subscribed to only French and some have subscribed to both newspapers.
+#
+# You are given two sets of student roll numbers.
+# One set has subscribed to the English newspaper, and the other set is subscribed to the French newspaper.
+# The same student could be in both sets.
+# Your task is to find the total number of students who have subscribed to at least one newspaper.
+
+if __name__ == '__main__':
+    size_a = int(input())
+    set_a = set(map(int, input().split()))
+    size_b = int(input())
+    set_b = set(map(int, input().split()))
+    print(len(set_a.union(set_b)))
