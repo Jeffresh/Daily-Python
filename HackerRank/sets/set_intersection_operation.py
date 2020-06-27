d9dd56d6905a9df761debf6be4c134542ee80370
Jeff <jeff.uca@gmail.com>
2020-06-27 10:24:43 +0200

    :sparkles: add hacker rank set intersection operation

---
+++ HackerRank/sets/set_intersection_operation.py
@@ -0,0 +1,18 @@
+# Task
+#
+# The students of District College have subscriptions to English and French newspapers.
+# Some students have subscribed only to English, some have subscribed only to French,
+# and some have subscribed to both newspapers.
+#
+# You are given two sets of student roll numbers.
+# One set has subscribed to the English newspaper, one set has subscribed to the French newspaper.
+# Your task is to find the total number of students who have subscribed to both newspapers.
+
+
+if __name__ == '__main__':
+    a_size = int(input())
+    set_a = set(map(int, input().split()))
+    b_size = int(input())
+    set_b = set(map(int, input().split()))
+
+    print(len(set_a.intersection(set_b)))
