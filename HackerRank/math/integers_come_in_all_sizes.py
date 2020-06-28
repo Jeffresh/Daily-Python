aad2d189965a84bc990e3c435dd7d983e5544de8
Jeff <jeff.uca@gmail.com>
2020-06-28 17:30:10 +0200

    :sparkles: add integers come in the all sizes hacker rank math

---
+++ HackerRank/math/integers_come_in_all_sizes.py
@@ -0,0 +1,10 @@
+# Task
+# Read four numbers a, b, c, and d, and print the result of a^b + c^d
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+
+    print(a ** b + c ** d)
