b5029b7afb3dc5a813897362d9c95e551acc8b98
Jeff <jeff.uca@gmail.com>
2020-07-01 08:51:28 +0200

    :sparkles: add counter hacker rank collection

---
+++ HackerRank/collections/counter.py
@@ -0,0 +1,24 @@
+# Task
+#
+# Raghu is a shoe shop owner. His shop has X number of shoes.
+# He has a list containing the size of each shoe he has in his shop.
+# There are N number of customers who are willing to pay xi
+# amount of money only if they get the shoe of their desired size.
+#
+# Your task is to compute how much money Raghu earned.
+from collections import Counter
+
+if __name__ == '__main__':
+    shoe_number = int(input())
+    shoes_size = list(map(int, input().split()))
+    customer_number = int(input())
+
+    counter_shoes = Counter(shoes_size)
+    earning = 0
+    for _ in range(customer_number):
+        size, cost = map(int, input().split())
+        if size in counter_shoes and counter_shoes[size] > 0:
+            earning += cost
+            counter_shoes[size] -= 1
+
+    print(earning)
