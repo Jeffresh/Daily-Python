6825cd7f21c623ea12e4ad8b185cf3481d515a93
Jeff <jeff.uca@gmail.com>
2020-07-04 17:14:37 +0200

    :sparkles: add exceptions hacker rank errors and exceptions

---
+++ HackerRank/exceptions/exceptions.py
@@ -0,0 +1,19 @@
+# Task
+#
+# You are given two values a and b,
+# Perform integer division and print a/b.
+
+# Input Format
+#
+# The first line contains T, the number of test cases.
+# The next T lines each contain the space separated values of a and b.
+
+if __name__ == '__main__':
+    test_cases = int(input())
+
+    for _ in range(test_cases):
+        try:
+            a, b = map(int, input().split())
+            print(a // b)
+        except (ZeroDivisionError, ValueError) as error:
+            print("Error Code:", error)
