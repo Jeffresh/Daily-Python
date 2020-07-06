223c8cc27c0de7fcf8dd1a2152e94b6ae2a7cfd2
Jeff <jeff.uca@gmail.com>
2020-07-06 20:02:13 +0200

    :sparkles: add eval function hacker rank built in

---
+++ HackerRank/built_ins/python_evaluation.py
@@ -0,0 +1,7 @@
+# Task
+# You are given an expression in a line.
+# Read that line as a string variable, such as var, and print the result using eval(var).
+
+if __name__ == '__main__':
+    instruction = input()
+    eval(instruction)
