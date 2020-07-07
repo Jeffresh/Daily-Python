f9ca1f38a26425ee31894d39c17ebc185fada585
Jeff <jeff.uca@gmail.com>
2020-07-07 15:24:48 +0200

    sparkles: add reduce_function hacker rank functions

---
+++ HackerRank/functionals/reduce_function.py
@@ -0,0 +1,27 @@
+# Input Format
+#
+# First line contains n
+# The ith of next lines contain two integers each, the numerator( Ni ) and denominator(Di ) of the ith rational number
+# in the list.
+
+# Output Format
+#
+# Print only one line containing the numerator and denominator of the product of the numbers in the list in
+# its simplest form, i.e. numerator and denominator have no common divisor other than 1
+
+
+from fractions import Fraction
+from functools import reduce
+
+
+def product(fracs):
+    t = reduce(lambda x, y: x * y, fracs)
+    return t.numerator, t.denominator
+
+
+if __name__ == '__main__':
+    fracs = []
+    for _ in range(int(input())):
+        fracs.append(Fraction(*map(int, input().split())))
+    result = product(fracs)
+    print(*result)
