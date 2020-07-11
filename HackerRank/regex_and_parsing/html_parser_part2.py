286c9bf4ab2ba6fafac408cbeccaa6d782d66fc1
Jeff <jeff.uca@gmail.com>
2020-07-11 10:02:32 +0200

    :sparkles: add hacker rank html parser part 2 regex and parsing task comments

--- HackerRank/regex_and_parsing/html_parser_part2.py
+++ HackerRank/regex_and_parsing/html_parser_part2.py
@@ -1,3 +1,31 @@
+# Task
+#
+# You are given an HTML code snippet of N lines
+
+# Your task is to print the single-line comments, multi-line comments and the data.
+#
+# Print the result in the following format:
+#
+# >>> Single-line Comment
+# Comment
+# >>> Data
+# My Data
+# >>> Multi-line Comment
+# Comment_multiline[0]
+# Comment_multiline[1]
+# >>> Data
+# My Data
+# >>> Single-line Comment:
+#
+
+# Note: Do not print data if data == '\n'.
+
+# Input Format
+#
+# The first line contains integer
+# , the number of lines in the HTML code snippet.
+# The next lines contains HTML code.
+
 from html.parser import HTMLParser
 
 
