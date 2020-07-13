5459ac4c1f596469834faa8324fe85c1e71eb5fe
Jeff <jeff.uca@gmail.com>
2020-07-13 19:02:33 +0200

    :art: correct some comments format

--- HackerRank/closures_and_decorators/name_directory.py
+++ HackerRank/closures_and_decorators/name_directory.py
@@ -2,7 +2,8 @@
 #
 # people. Each person has a first name, last name, age and sex.
 # Print their names in a specific format sorted by their age in ascending order i.e.
-# the youngest person's name should be printed first. For two people of the same age, print them in the order of their input.
+# the youngest person's name should be printed first. For two people of the same age,
+# print them in the order of their input.
 #
 # For Henry Davids, the output should be:
 #
