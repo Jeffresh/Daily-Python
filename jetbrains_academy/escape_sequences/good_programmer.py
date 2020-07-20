5515281c91f01cec27124595397d83e28cff3a4c
Jeff <jeff.uca@gmail.com>
2020-07-20 13:40:59 +0200

    :sparkles: jetbrains academy good programmer escape sequences

---
+++ jetbrains_academy/escape_sequences/good_programmer.py
@@ -0,0 +1,8 @@
+# You are really good at what you do (programming), so your colleague has decided to congratulate
+# you on the completed project with a message "You are the best programmer!". But how to do this?
+#
+# Write the code that prints this message using an escape sequence
+# so that each word of the sentence would be on a new line.
+
+if __name__ == '__main__':
+    print('You\n are\n the\n best\n programmer!\n')
