5b840bb013453ff31180cdaedd0d1067f2aaee86
Jeff <jeff.uca@gmail.com>
2020-07-20 13:35:02 +0200

    :sparkles: jetbrains academy find the length escape sequences

---
+++ jetbrains_academy/escape_sequences/find_the_length.py
@@ -0,0 +1,5 @@
+# Write a program that will print the length of the string 'That is \n mine'.
+# The single quotes are not part of this string.
+
+if __name__ == '__main__':
+    print(len('That is \n mine'))
