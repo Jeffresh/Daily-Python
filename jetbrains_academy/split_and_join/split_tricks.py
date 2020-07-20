a1511de34ff1961deac7216ac7c69fb6824aa3bd
Jeff <jeff.uca@gmail.com>
2020-07-20 17:27:20 +0200

    :sparkles: jetbrains academy string tricks split and join

---
+++ jetbrains_academy/split_and_join/split_tricks.py
@@ -0,0 +1,4 @@
+# Examine the code and fix the mistakes so that the join() method works.
+if __name__ == '__main__':
+    random_numbers = [1, 22, 333, 4444, 55555]
+    print("\n".join([str(x) for x in random_numbers]))
