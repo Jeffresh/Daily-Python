db2c4359433fc50999436ad021f169331775554b
Jeff <jeff.uca@gmail.com>
2020-07-22 17:38:15 +0200

    :sparkles: jetbrains academy oceans tuple

---
+++ jetbrains_academy/tuple/oceans.py
@@ -0,0 +1,6 @@
+# Turn a list oceans into a tuple to prevent further data change.
+#
+# You are NOT supposed to create a new variable for the tuple or print anything.
+
+if __name__ == '__main__':
+    oceans = tuple(['Atlantic', 'Pacific', 'Indian', 'Southern', 'Arctic'])
