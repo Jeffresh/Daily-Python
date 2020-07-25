50f6d6c75d96dbbb52ad962b9eb1fe1f34ede6d5
Jeff <jeff.uca@gmail.com>
2020-07-25 11:00:57 +0200

    :sparkles: jetbrains academy levitate search in a string

---
+++ jetbrains_academy/search_in_a_string/levitate.py
@@ -0,0 +1,7 @@
+# Find an input substring in the spell "Wingardium Leviosa" and print its starting index.
+#
+# If there's no such substring, output -1.
+
+if __name__ == '__main__':
+    spell = "Wingardium Leviosa"
+    print(spell.find(input()))
