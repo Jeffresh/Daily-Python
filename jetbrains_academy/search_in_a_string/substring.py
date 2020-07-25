b7737fbcac5b1b7c46447e63a5116fc763b9dbf7
Jeff <jeff.uca@gmail.com>
2020-07-25 11:18:53 +0200

    :sparkles: jetbrains academy substring search in a string

---
+++ jetbrains_academy/search_in_a_string/substring.py
@@ -0,0 +1,10 @@
+# Write a program that takes two strings, checks whether the first string contains
+# the second one and prints the result of the membership test.
+#
+# The variables for both input strings have already been defined.
+
+if __name__ == '__main__':
+    a = input()
+    b = input()
+    # run the membership test below
+    print(b in a)
