c1d35d4c4a205858e7cb69c9a0ba298fd4483260
Jeff <jeff.uca@gmail.com>
2020-08-04 22:41:54 +0200

    :sparkles: Jetbrains academy check references objects in python

---
+++ jetbrains_academy/objects_in_python/check_references.py
@@ -0,0 +1,15 @@
+# Imagine, you have two variables and you don't know what they contain but need to check if
+# they refer to the same object or not. How would you do that?
+#
+# Write a function check(obj1, obj2) that will take two objects and print True if they refer to
+# the same object and False otherwise.
+
+
+def check(obj1, obj2):
+    print(id(obj1) == id(obj2))
+
+
+if __name__ == '__main__':
+    a = list()
+    b = a
+    check(a, b)
