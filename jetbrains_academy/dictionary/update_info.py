2fb64fad7eaa464d584eb3a7742cc1bc54fb4bc2
Jeff <jeff.uca@gmail.com>
2020-08-12 11:15:15 +0200

    :sparkles: JetBrains Academy update info dictionary

---
+++ jetbrains_academy/dictionary/update_info.py
@@ -0,0 +1,12 @@
+# Let's say you have a dict with children's names and professions they would like to have when they grow up:
+#
+# children = {'Emily': 'artist', 'Adam': 'astronaut', 'Nancy': 'programmer'}
+#
+# But what if someone's choice has changed? Say, Emily now wants to be a musician.
+# Update the dict and print the new version.
+
+
+if __name__ == '__main__':
+    children = {'Emily': 'artist', 'Adam': 'astronaut', 'Nancy': 'programmer'}
+    children['Emily'] = 'musician'
+    print(children)
