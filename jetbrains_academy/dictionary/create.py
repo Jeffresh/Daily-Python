583ccc3264a06487dd843d02f1b49d64881c6881
Jeff <jeff.uca@gmail.com>
2020-08-12 11:12:37 +0200

    :sparkles: JetBrains Academy create dictionary

---
+++ jetbrains_academy/dictionary/create.py
@@ -0,0 +1,9 @@
+# Let's say you asked your friends to name their favorite flowers: now you know that Alex likes 'field flowers',
+# Kate prefers 'daffodil', Eva's favorite is 'artichoke flower', and Daniel loves 'tulip'.
+#
+# Create a dict with the names as keys and flowers as values and print it.
+
+
+if __name__ == '__main__':
+    friends = {'Alex': 'field flowers', 'Kate': 'daffodil', 'Eva': 'artichoke flower', 'Daniel': 'tulip'}
+    print(friends)
