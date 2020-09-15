a15306822cea956754fd2a2bd96b492edacf5198
Jeffresh <jeff.uca@gmail.com>
2020-09-15 21:23:34 +0200

    :sparkles: jetbrains collections slicing reverse

---
+++ jetbrains_academy/collections/reverse.py
@@ -0,0 +1,8 @@
+# The code below is supposed to choose elements with indexes from 4 to 16 inclusively in reverse order,
+# but a mistake has been made. Find it and fix it.
+
+
+if __name__ == '__main__':
+    numbers = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20,
+               22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
+    print(numbers[16:3:-1])
