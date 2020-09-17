91e14c17e48b473d9161acf4ccc611fe7c4430fe
Jeffresh <jeff.uca@gmail.com>
2020-09-17 10:42:55 +0200

    :sparkles: jetbrains quotes and multi-line strings, triple quoted string

---
+++ jetbrains_academy/strings/triple_quoted_string.py
@@ -0,0 +1,10 @@
+# Create a program that will print this multi-line string containing triple double quotes:
+#
+# """
+# THIS IS A STRING
+# """
+#
+# Note: quotes, too, should be printed!
+
+if __name__ == '__main__':
+    print("\"\"\"\nTHIS IS A STRING\n\"\"\"")
