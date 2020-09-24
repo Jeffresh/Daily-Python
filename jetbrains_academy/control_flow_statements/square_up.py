6ea98134e12cee8ee7e8c307bdee97c63141ec87
Jeffresh <manuel.pallanunez@alum.uca.es>
2020-09-24 16:54:21 +0200

    :sparkles: add jetbrains control flow statements while loop square up

---
+++ jetbrains_academy/control_flow_statements/square_up.py
@@ -0,0 +1,9 @@
+# The program below is supposed to print the squares of all numbers from 1 to 20. but there are some
+# mistakes. Find them and fix them.
+
+
+if __name__ == '__main__':
+    i = 1
+    while i <= 20:
+        print(i)
+        i += 1
