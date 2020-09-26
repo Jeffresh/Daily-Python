e64942becaddbd14704e50ef4220bc57542a0f53
Jeffresh <manuel.pallanunez@alum.uca.es>
2020-09-26 20:17:14 +0200

    :sparkles: builtins sep jetbrains print several lines one print

---
+++ jetbrains_academy/builtins/print/several_lines_one_print.py
@@ -0,0 +1,19 @@
+# write a snippet that will print the following lines using only one print() statement
+#
+# The lines:
+#
+# Night, square, apothecary,lantern,
+# Its meaningless and pallid light.
+# Return a half a lifetime after -
+# All will remain. A scapeless rite.
+#
+
+
+if __name__ == '__main__':
+    line1 = "Night, square, apothecary, lantern,"
+    line2 = "Its meaningless and pallid light."
+    line3 = "Return a half a lifetime after -"
+    line4 = "All will remain. A scapeless rite."
+
+    # your one print() statement here
+    print(line1, line2, line3, line4, sep='\n')
