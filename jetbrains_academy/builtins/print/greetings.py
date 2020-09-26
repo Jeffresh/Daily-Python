4230c18a20d00983355f94aa0a1a12feb2b6c4d6
Jeffresh <manuel.pallanunez@alum.uca.es>
2020-09-26 20:33:07 +0200

    :sparkles: builtins sep jetbrains print greetings

---
+++ jetbrains_academy/builtins/print/greetings.py
@@ -0,0 +1,21 @@
+# Imagine you have string variables:
+#
+# adj = "Good"
+# part_of_day = "morning"
+# comma = ","
+# title = "Ms."
+# surname = "Johnson"
+#
+# And you need to print out a greeting "Good morning, Ms. Johnson!" (without the quotes) calling the print() function
+# only once and specifying its keyword arguments, without adding any other strings. How would you do that?
+
+
+if __name__ == '__main__':
+    adj = "Good"
+    part_of_day = "morning"
+    comma = ","
+    title = "Ms."
+    surname = "Johnson"
+
+    # your print() function
+    print(adj, part_of_day+comma, title, surname, end='!')
