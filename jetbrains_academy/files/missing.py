e038a9d1396c48049c9f28f157d26e18d2ad3ea0
Jeffresh <manuel.pallanunez@alum.uca.es>
2020-09-27 11:19:28 +0200

    :sparkles: builtins sep jetbrains file: missing

---
+++ jetbrains_academy/files/missing.py
@@ -0,0 +1,8 @@
+# Below you can see a code snippet for working with a file. Howver, a crucial line of code is missing.
+# Add the missing line to the code.
+
+if __name__ == '__main__':
+    file = open('test_file.txt', 'r')
+    print('Opened test_file!')
+    # missing line
+    file.close()
