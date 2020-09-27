0316fcbcb4ab7d9e39de401a6e8fda73428465ea
Jeffresh <manuel.pallanunez@alum.uca.es>
2020-09-27 11:13:16 +0200

    :sparkles: builtins sep jetbrains file: error

---
+++ jetbrains_academy/files/error.py
@@ -0,0 +1,11 @@
+# Alice has a file called "books.txt"with all the books that she nneds to read for her class. Her professor added
+# another book to the list, so Alice wants to add a new line to the file. Below you can see her code.
+#
+# However, Alice has made a mistake and got a completely different result than expected. Fix her mistake.
+# Don't worry about closing the file here, it will be done under the hood.
+
+if __name__ == '__main__':
+    # find a mistake below
+    file = open('books.txt', 'a', encoding='utf-8')
+
+    #closing and other operations are done below
