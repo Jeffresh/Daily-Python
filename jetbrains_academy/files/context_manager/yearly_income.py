99dee98842e202b6c30da73bd6c26cfd9909f91b
Jeffresh <manuel.pallanunez@alum.uca.es>
2020-09-28 10:17:18 +0200

    :sparkles: jetbrains files - context manager: yearly income

---
+++ jetbrains_academy/files/context_manager/yearly_income.py
@@ -0,0 +1,20 @@
+# The file salary.txt contains a monthly salary of all employees in the company.
+# Assume that the monthly salary is fixed and each employee is mentioned once. So,
+# each number (an integer) written on a separate line corresponds to a particular employee:
+
+# 3500
+# 4780
+# 6666
+# ...
+#
+# According to the example, the first worker gets 3,500 per month, the second one 4,780 and the third 6,666, etc.
+#
+# Calculate how much each employee earns per year and save their yearly income to a file salary_year.txt.
+# Similarly to the original file, each income should be on a separate line. Preserve the order as
+# it helps identify an employee.
+
+if __name__ == '__main__':
+
+    with open('salary.txt', mode='r') as salary_file, open('salary_year.txt', mode='w') as salary_year_file:
+        for line in salary_file:
+            salary_year_file.write(str(int(line) * 12) + '\n')
