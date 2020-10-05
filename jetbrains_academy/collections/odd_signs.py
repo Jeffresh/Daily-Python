671737288757764f41107aad8a566ad5d4115deb
jeffresh <jeff.uca@gmail.com>
2020-10-05 07:46:36 +0200

    :sparkles: jetbrains collections slicing: odd_signs

---
+++ jetbrains_academy/collections/odd_signs.py
@@ -0,0 +1,6 @@
+# There is a variable word defined below. It stores and input string. Print only its symbols with
+# odd indexes.
+
+if __name__ == '__main__':
+    word = input()
+    print(word[1::2])
